b20a59ca26b4a5a03018aedfbfa9b9e987b39ea8
Руслан <Stariy123@mail.ru>
2023-07-06 23:13:15 +0300

    Initial commit

---
+++ src/ObjectSpawner.java
@@ -0,0 +1,12 @@
+public class ObjectSpawner <C>{
+    private Class <C> clazz;
+
+    public ObjectSpawner(Class<C> classC) {
+        this.clazz = clazz;
+
+    }
+    public C create() throws InstantiationException, IllegalAccessException {
+        return clazz.newInstance();
+    }
+
+}
